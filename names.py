95f3a67a225188bcc072880600bd3f5266876402
gtt116 <gtt116@gmail.com>
2016-09-07 15:56:40 +0800

    add names mapping

---
+++ names.py
@@ -0,0 +1,9 @@
+# -*-coding:utf8-*-
+"""
+This file contains all name mapping from English to Chinese.
+"""
+
+NAMES = {
+    'MeetDroidFriends': '与星球大战里的机器人朋友见面/星球大战远征基地/银河帝国贸易站',
+    'TronLightcyclePowerRun': '创急速光轮',
+}
