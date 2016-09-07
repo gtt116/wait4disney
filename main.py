984189a4e25add6c16e861b3763e05cc05fc579f
gtt116 <gtt116@gmail.com>
2016-09-07 15:45:11 +0800

    write data into grafana

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
 import disney_api as api
 
+import influxdb
 
-for a in api.attractions():
-    print a
+
+def main():
+    host = 'localhost'
+    port = 8086
+    user = 'root'
+    password = 'root'
+    dbname = 'disney'
+
+    db = influxdb.InfluxDBClient(host, port, user, password, dbname)
+    db.create_database(dbname)
+
+    datapoints = []
+    for a in api.attractions():
+        datapoint = {
+            "measurement": "wait_minutes",
+            "tags": {
+                "name": a.name,
+            },
+            "fields": {
+                "value": a.wait_minutes,
+            },
+        }
+        datapoints.append(datapoint)
+        print datapoint
+
+    print db.write_points(datapoints, database='disney', batch_size=50)
+
+
+if __name__ == '__main__':
+    main()
