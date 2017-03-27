270b9e7e0cc12c7cd3fd02126d7f55743074d7b4
gtt116 <gtt116@gmail.com>
2017-03-27 22:36:36 +0800

    add subcommand into web.py

--- web.py
+++ web.py
@@ -1,8 +1,10 @@
-from datetime import datetime
-import collections
+import sys
 import json
+from datetime import datetime
+from datetime import timedelta
 
 import main as backend
+import names as name_map
 
 
 def to_timestamp(time_str):
@@ -10,12 +12,20 @@ def to_timestamp(time_str):
     return d.strftime('%s')
 
 
-def main():
+def format_date(dt):
+    return dt.strftime('%Y-%m-%d')
+
+
+def year():
     db = backend.get_influxdb()
 
-    result = db.query("""SELECT sum("value") FROM "wait_minutes" where time >= '2016-09-07T00:00:00Z' GROUP BY time(1d) fill(null);""")
-
-    payload = collections.OrderedDict()
+    result = db.query(
+        """
+        SELECT
+        sum("value")
+        FROM "wait_minutes"
+        where time >= '2016-09-07' GROUP BY time(1d) fill(0);
+        """)
 
     top = 0
 
@@ -33,8 +43,77 @@ def main():
         value = 1000 * sum_value / top
         datas.append([time, value])
 
-    print json.dumps(datas, indent=2)
+    print json.dumps(datas)
+
+
+def get_day_top(date=None, top=25):
+    if date is None:
+        date = datetime.now()
+        date = date.replace(hour=0, minute=0, second=0, microsecond=0)
+
+    db = backend.get_influxdb()
+    start = date
+    end = date + timedelta(days=1)
+    iql = '''
+        SELECT
+            max("value") as max,
+            mean("value") as mean
+        FROM wait_minutes
+        WHERE time >= '%(start)s' and time < '%(end)s'
+        GROUP by "name"
+        ''' % {'start': format_date(start), 'end': format_date(end)}
+    result = db.query(iql)
+
+    stats = []
+    for item in result.items():
+        name = name_map.translate(item[0][1]['name'])
+        datas = next(item[1])
+        mean = datas['mean']
+        max_ = datas['max']
+        stats.append([name, int(mean), int(max_)])
+
+    # sort by mean number
+    stats.sort(key=lambda x: x[2], reverse=True)
+    stats = stats[:top]
+    stats.reverse()
+
+    games = []
+    mean_values = []
+    max_values = []
+
+    for row in stats:
+        games.append(row[0])
+        mean_values.append(row[1])
+        max_values.append(row[2])
+
+    ret = {
+        'date': format_date(date),
+        'games': games,
+        'max': max_values,
+        'mean': mean_values
+    }
+    print json.dumps(ret)
+
+
+def many_days():
+    dt = datetime(2016, 9, 7)
+    while dt <= datetime(2017, 3, 27):
+        ret = get_day_top(dt)
+        with file('%s.json' % dt.strftime('%Y-%m-%d'), 'w') as output:
+            print output
+            output.write(ret)
+        dt += timedelta(days=1)
 
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) != 2:
+        print "Usage: %s <year|day|many_days>" % sys.argv[0]
+        sys.exit(1)
+
+    cmd = sys.argv[1]
+    cmd_map = {
+        'year': year,
+        'day': get_day_top,
+        'many_days': many_days,
+    }
+    cmd_map[cmd]()
--- web.sh
+++ web.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-python `dirname $0`/web.py > /var/www/static/disney/disney.json
+python `dirname $0`/web.py year > /var/www/static/disney/year.json
+
+today=`date +%Y-%m-%d`
+python `dirname $0`/web.py day > /var/www/static/disney/latest.json
+cp /var/www/static/disney/latest.json /var/www/static/disney/${today}.json
