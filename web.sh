c643dcb35c74d193cacfa28fff3ebca90254867b
gtt116 <gtt116@gmail.com>
2017-09-16 17:23:06 +0800

    修复了计算过程中year.json等文件变空的bug

--- web.sh
+++ web.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-python `dirname $0`/web.py year > /var/www/static/disney/year.json
+home=/var/www/static/disney/
+
+# because pipe redirect will clean the file before invoke the command,
+# if command runs for a long time, the file will keep empty. This method
+# work around it.
+python `dirname $0`/web.py year > $home/year_new.json
+mv $home/year_new.json $home/year.json
+
+python `dirname $0`/web.py day > $home/latest_new.json
 
 today=`date +%Y-%m-%d`
-python `dirname $0`/web.py day > /var/www/static/disney/latest.json
-cp /var/www/static/disney/latest.json /var/www/static/disney/${today}.json
+cp $home/latest_new.json $home/latest.json
+mv $home/latest_new.json $home/${today}.json
